2aaa0bba21b8ba9418b0276340b78e7d9c9bffcd
souravpal01 <souravpal7217@gmail.com>
2023-02-07 12:02:17 +0530

    customer microservice

--- customer/src/main/java/customer/CustomerService.java
+++ customer/src/main/java/customer/CustomerService.java
@@ -3,12 +3,14 @@ package customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void register(CustomerRequest customerRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRequest.firstName())
                 .lastName(customerRequest.lastName())
                 .email(customerRequest.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }
