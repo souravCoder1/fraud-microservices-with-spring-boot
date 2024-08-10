af80ece3370e8aeda47610143cc4571b1ae87f80
souravpal01 <souravpal7217@gmail.com>
2024-08-10 15:31:22 +0530

    refactor producer

--- amqp/src/main/java/amqp/RabbitMQMessageProducer.java
+++ amqp/src/main/java/amqp/RabbitMQMessageProducer.java
@@ -5,17 +5,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Component;
 
+/**
+ * This component is responsible for publishing messages to RabbitMQ.
+ */
 @Component
 @Slf4j
 @AllArgsConstructor
 public class RabbitMQMessageProducer {
 
+    // The AmqpTemplate is injected, which will be used to send messages to RabbitMQ.
     private final AmqpTemplate amqpTemplate;
 
+    /**
+     * Publishes a message to the specified exchange with the given routing key.
+     * @param payload The message to be sent.
+     * @param exchange The name of the exchange to send the message to.
+     * @param routingKey The routing key to use for directing the message.
+     */
     public void publish(Object payload, String exchange, String routingKey) {
+        // Log the details before sending the message.
         log.info("Publishing to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
+
+        // Send the message to the specified exchange with the routing key.
         amqpTemplate.convertAndSend(exchange, routingKey, payload);
+
+        // Log the details after the message has been sent.
         log.info("Published to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
     }
-
 }
