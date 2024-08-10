256a55a8d3622dc9274c4139f8f53bd22c386b27
souravpal01 <souravpal7217@gmail.com>
2024-08-10 15:39:17 +0530

    refactor NotificationConsumer

--- notification/src/main/java/notification/rabbitmq/NotificationConsumer.java
+++ notification/src/main/java/notification/rabbitmq/NotificationConsumer.java
@@ -1,20 +1,22 @@
-//
+package notification.rabbitmq;
 
-//import lombok.AllArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@AllArgsConstructor
-//@Slf4j
-//public class NotificationConsumer {
-//
-//    private final NotificationService notificationService;
-//
-//    @RabbitListener(queues = "${rabbitmq.queues.notification}")
-//    public void consumer(NotificationRequest notificationRequest) {
-//        log.info("Consumed {} from queue", notificationRequest);
-//        notificationService.send(notificationRequest);
-//    }
-//}
+import clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import notification.NotificationService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queues.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed {} from queue", notificationRequest);
+        notificationService.send(notificationRequest);
+    }
+}
